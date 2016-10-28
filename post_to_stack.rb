50393bfcfbf0665d69cd2e09341f58674273ab0b
cuong <cuong@maruweb.co.jp>
2016-10-28 21:33:00 +0900

    post to stackoverflow

--- post_to_stack.rb
+++ post_to_stack.rb
@@ -1,29 +1,52 @@
-ENV["SSL_CERT_FILE"] = "./cacert.pem" #OpenSSLの対策です。
-require "httparty"
 require "slack"
 require "yaml"
+require_relative  "stack"
 
-$site = 'stackoverflow'
-$access_token = '' #取得したaccess_token
-$title = '' #投稿するタイトル
-$body = '' #投稿する本文
-$tags = '' #投稿するタグ
-$key = '' #登録したstackappのキー
-$preview = true #テストだけにしたければtrueにして下さい。
+include StackOverFlow
 
-class SOF
-  include HTTParty
-  base_uri "https://api.stackexchange.com"
+setting = YAML.load_file("settings.yml")
+TOKEN = setting["token"]
+puts TOKEN
 
-  def questions
-    self.class.post("/2.2/questions/add", 
-    { body: {site: $site, title: $title, body: $body, tags: $tags, key: $key, access_token: $access_token, preview: $preview} })
-  end
+BOTNAME = "test-bot"
+Slack.configure {|config| config.token = TOKEN }
+client = Slack.realtime
 
+client.on :hello do
+  puts 'Successfully connected.'
 end
 
-sof = SOF.new()
-#StackOverFlowは投稿タイトルと本文をよくチェックするので、ご注意ください。
-#投稿タイトルと本文が不合格になれば、エラーメッセージ："This post does not meet our quality standards."が出ます。
-puts sof.questions
+client.on :message do |data|
+  if data.key?('text')
+    if data['text']['&lt;post_to_stack&gt;'] && data['subtype'] != 'bot_message'
+      $title = data['text']['&lt;title&gt;'] ? data['text'][data['text'].index('&lt;title&gt;') + ('&lt;title&gt;').length..data['text'].index('&lt;end_title&gt;') - 1] : ''
+      $body = data['text']['&lt;body&gt;'] ? data['text'][data['text'].index('&lt;body&gt;') + ('&lt;body&gt;').length..data['text'].index('&lt;end_body&gt;') - 1] : ''
+      $tags = data['text']['&lt;tags&gt;'] ? data['text'][data['text'].index('&lt;tags&gt;') + ('&lt;tags&gt;').length..data['text'].index('&lt;end_tags&gt;') - 1] : ''
+      $access_token = data['text']['&lt;access_token&gt;'] ? data['text'][data['text'].index('&lt;access_token&gt;') + ('&lt;access_token&gt;').length..data['text'].index('&lt;end_access_token&gt;') - 1] : ''
+      $key = data['text']['&lt;key&gt;'] ? data['text'][data['text'].index('&lt;key&gt;') + ('&lt;key&gt;').length..data['text'].index('&lt;end_key&gt;') - 1] : ''
+      $preview = data['text']['&lt;preview&gt;'] ? data['text'][data['text'].index('&lt;preview&gt;') + ('&lt;preview&gt;').length..data['text'].index('&lt;end_preview&gt;') - 1] : ''
+      sof_message = $title + "\n" + $body + "\n" + $tags + "\n" + $access_token + "\n" + $key + "\n" + $preview
+      puts sof_message
+      
+      sof = SOF.new()
+      #StackOverFlowは投稿タイトルと本文をよくチェックするので、ご注意ください。
+      #投稿タイトルと本文が不合格になれば、エラーメッセージ："This post does not meet our quality standards."が出ます。
+      result = sof.questions
+      
+      
+      params = {
+        token: TOKEN,
+        channel: data['channel'],
+        text: "<@#{data['user']}> \n" + sof_message} 
+      Slack.chat_postMessage params
+
+    end
+  end
+
+  
+end
+
+client.start
+Process.daemon
+
 
---
+++ stack.rb
@@ -0,0 +1,28 @@
+#stack.rb
+require "httparty"
+
+module StackOverFlow
+  
+  $site = 'stackoverflow'
+  $access_token = '' #取得したaccess_token
+  $title = '' #投稿するタイトル
+  $body = '' #投稿する本文
+  $tags = '' #投稿するタグ
+  $key = '' #登録したstackappのキー
+  $preview = true #テストだけにしたければtrueにして下さい。
+  
+  class SOF
+    include HTTParty
+    base_uri "https://api.stackexchange.com"
+  
+    def questions
+      self.class.post("/2.2/questions/add", 
+      { body: {site: $site, title: $title, body: $body, tags: $tags, key: $key, access_token: $access_token, preview: $preview} })
+    end
+  
+  end
+
+  
+end
+
+
